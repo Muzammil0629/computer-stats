060df42ca3f7a92fce00a165a8c775d0a183090a
muzammil0629 <muzammil0626@gmail.com>
2026-05-29 17:15:59 -0400

    Installed Nginx

--- app.py
+++ app.py
@@ -2,21 +2,33 @@ from flask import Flask
 import socket
 import datetime
 import shutil
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
     total, used, free = shutil.disk_usage("/")
+
     return f"""
-    <h1>Computer Stats Dashboard</h1>
-    <p><b>Hostname:</b> {socket.gethostname()}</p>
-    <p><b>Current Time:</b> {datetime.datetime.now()}</p>
-    <p><b>Disk Total:</b> {total // (2**30)} GB</p>
-    <p><b>Disk Used:</b> {used // (2**30)} GB</p>
-    <p><b>Disk Free:</b> {free // (2**30)} GB</p>
-    <p><b>Status:</b> Running from Docker on AWS EC2</p>
+    <html>
+    <head>
+        <title>Computer Stats Dashboard</title>
+    </head>
+    <body>
+        <h1>Computer Stats Dashboard</h1>
+
+        <p><b>Version:</b> 2.0</p>
+        <p><b>Hostname:</b> {socket.gethostname()}</p>
+        <p><b>Current Time:</b> {datetime.datetime.now()}</p>
+
+        <p><b>Disk Total:</b> {total // (2**30)} GB</p>
+        <p><b>Disk Used:</b> {used // (2**30)} GB</p>
+        <p><b>Disk Free:</b> {free // (2**30)} GB</p>
+
+        <p><b>Status:</b> Healthy</p>
+        <p><b>Deployment:</b> Automated via GitHub Actions</p>
+    </body>
+    </html>
     """
 
 if __name__ == "__main__":
